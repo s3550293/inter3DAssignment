e08d0b576a74f8dc5c025c8fe15484cfb25baed6
Joseph Garner <s3550293@student.rmit.edu.au>
2018-03-30 14:56:28 +1100

    Changing code back for testing

--- prim.c
+++ prim.c
@@ -33,30 +33,18 @@ void display()
 }
 
 void drawLine(float x){
-    // const int n = 4;
-    // const float r = 1.0;
-    // float x, y;
+    const int n = 20;
+    const float r = 1.0;
+    float theta;
+    float x, y;
 
-    // glPointSize(2.0);
-    // glBegin(GL_POINTS);
-    // for (int i = 0; i <= n; i++) {
-    //     x = (i / (float)n - 0.5) * 2.0 * r;
-    //     y = sqrtf(r * r - x * x);
-    //     glVertex3f(x, y);
-    //     glVertex3f(x, -y);
-    // }
-    // glEnd();
-
-    const int c = 10;
-    const float m = 1;
-    float y;
-    glPointSize(2.0);
-    glColor3f(0,0,0);
-    glBegin(GL_POINTS);
-    for (int i = 0; i<= c; i++){
-        y = (x * m + i);
-        x = x*m;
-        glVertex3f(x, y, 0);
+    glLineWidth(2.0);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i <= n; i++) {
+        theta = i / (float)n * 2.0 * M_PI;
+        x = r * cosf(theta);
+        y = r * sinf(theta);
+        glVertex2f(x, y);
     }
     glEnd();
 }
